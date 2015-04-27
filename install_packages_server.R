6052b45e5d6b6dd0b036ebd60477ed9409f70112
Saw Simeon <doe_doe207@hotmail.com>
2015-04-27 16:38:14 +0700

    packages for Rstudio server

---
+++ install_packages_server.R
@@ -0,0 +1,12 @@
+library(RCurl)
+set.seed(3)
+x <- getURL("https://raw.githubusercontent.com/Rnewbie/OliFP/master/packages_to_be_install.csv")
+packages <- read.csv(text=x, header = TRUE)
+bio <- as.character(packages$bioconductor)
+cran <- as.character(packages$CRAN)
+##to install packages from bioconductor
+source("http://bioconductor.org/biocLite.R")
+biocLite(bio)
+## to install packages from CRAN
+install.packages(cran)
+
