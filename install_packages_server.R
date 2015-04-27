89806db18c1ecb9103f6e19a2f0d896551d7f43a
Saw Simeon <Rnewbie@users.noreply.github.com>
2015-04-27 22:33:01 +0700

    Update install_packages_server.R

--- install_packages_server.R
+++ install_packages_server.R
@@ -2,7 +2,7 @@ library(RCurl)
 set.seed(3)
 x <- getURL("https://raw.githubusercontent.com/Rnewbie/OliFP/master/packages_to_be_install.csv")
 packages <- read.csv(text=x, header = TRUE)
-bio <- as.character(packages$bioconductor)
+bio <- as.character(packages$Bioconductor)
 cran <- as.character(packages$CRAN)
 ##to install packages from bioconductor
 source("http://bioconductor.org/biocLite.R")
